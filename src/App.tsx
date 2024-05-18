69d2274e9d367b7157ae1f9146e169b2ec578397
TA-800 <taherali0905@gmail.com>
2024-05-18 01:41:30 -0400

    added clear all btn

--- src/App.tsx
+++ src/App.tsx
@@ -96,14 +96,14 @@ function App() {
     SELECT,
     BRUSH,
     ERASER,
+    CLEAR,
   }
   const selectToolbarButton = (button: ToolbarButton) => {
     saveFrameState();
 
-    mainFabRef.current!.isDrawingMode = false;
-
     switch (button) {
       case ToolbarButton.SELECT:
+        mainFabRef.current!.isDrawingMode = false;
         setSelectedTool("select");
         break;
       case ToolbarButton.BRUSH:
@@ -112,6 +112,9 @@ function App() {
         break;
       case ToolbarButton.ERASER:
         // TODO
+        break;
+      case ToolbarButton.CLEAR:
+        clearCanvas();
         break;
     }
   };
@@ -136,13 +139,17 @@ function App() {
 
   // frame rendering on current frame change
   useEffect(() => {
-    if (mainFabRef.current) {
-      if (!mainFabRef.current.isEmpty() && frames[currentFrame] === null) {
-        clearCanvas();
-        return;
-      }
+    // Render current frame's drawing state
+    if (!mainFabRef.current) return;
+
+    // if current frame is null, clear canvas. Useful when adding new frame.
+    // throws error "ctx is null" if isEmpty check not done before calling clear here
+    if (!mainFabRef.current.isEmpty() && frames[currentFrame] === null) {
+      clearCanvas();
+      return;
     }
 
+    // else, load drawing state from frames[currentFrame]
     mainFabRef.current?.loadFromJSON(frames[currentFrame], () => {
       mainFabRef.current!.renderAll();
     });
@@ -292,6 +299,23 @@ function App() {
             <path
               fillRule="evenodd"
               d="M20.599 1.5c-.376 0-.743.111-1.055.32l-5.08 3.385a18.747 18.747 0 0 0-3.471 2.987 10.04 10.04 0 0 1 4.815 4.815 18.748 18.748 0 0 0 2.987-3.472l3.386-5.079A1.902 1.902 0 0 0 20.599 1.5Zm-8.3 14.025a18.76 18.76 0 0 0 1.896-1.207 8.026 8.026 0 0 0-4.513-4.513A18.75 18.75 0 0 0 8.475 11.7l-.278.5a5.26 5.26 0 0 1 3.601 3.602l.502-.278ZM6.75 13.5A3.75 3.75 0 0 0 3 17.25a1.5 1.5 0 0 1-1.601 1.497.75.75 0 0 0-.7 1.123 5.25 5.25 0 0 0 9.8-2.62 3.75 3.75 0 0 0-3.75-3.75Z"
+              clipRule="evenodd"
+            />
+          </svg>
+        </button>
+        <button
+          className="btn"
+          onClick={() => selectToolbarButton(ToolbarButton.CLEAR)}
+        >
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 24 24"
+            fill="currentColor"
+            className="w-6 h-6"
+          >
+            <path
+              fillRule="evenodd"
+              d="M16.5 4.478v.227a48.816 48.816 0 0 1 3.878.512.75.75 0 1 1-.256 1.478l-.209-.035-1.005 13.07a3 3 0 0 1-2.991 2.77H8.084a3 3 0 0 1-2.991-2.77L4.087 6.66l-.209.035a.75.75 0 0 1-.256-1.478A48.567 48.567 0 0 1 7.5 4.705v-.227c0-1.564 1.213-2.9 2.816-2.951a52.662 52.662 0 0 1 3.369 0c1.603.051 2.815 1.387 2.815 2.951Zm-6.136-1.452a51.196 51.196 0 0 1 3.273 0C14.39 3.05 15 3.684 15 4.478v.113a49.488 49.488 0 0 0-6 0v-.113c0-.794.609-1.428 1.364-1.452Zm-.355 5.945a.75.75 0 1 0-1.5.058l.347 9a.75.75 0 1 0 1.499-.058l-.346-9Zm5.48.058a.75.75 0 1 0-1.498-.058l-.347 9a.75.75 0 0 0 1.5.058l.345-9Z"
               clipRule="evenodd"
             />
           </svg>
