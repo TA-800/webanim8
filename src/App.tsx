23584faf90fcbe26edc4518d2b16bab32c3ef628
TA-800 <taherali0905@gmail.com>
2024-05-17 23:14:14 -0400

    init canvases

--- src/App.tsx
+++ src/App.tsx
@@ -8,14 +8,34 @@ function App() {
   const bgFabRef = useRef<fabric.Canvas | null>(null);
 
   // Canvas references
-  const mainCanvasRef = useRef<HTMLCanvasElement | null>(null);
-  const onionCanvasRef = useRef<HTMLCanvasElement | null>(null);
-  const bgCanvasRef = useRef<HTMLCanvasElement | null>(null);
+  const mainCanvasRef = useRef(null);
+  const onionCanvasRef = useRef(null);
+  const bgCanvasRef = useRef(null);
+
+  // frame rendering on current frame change
+  // TODO
+
+  // initialization
+  useEffect(() => {
+    mainFabRef.current = new fabric.Canvas(mainCanvasRef.current);
+    onionFabRef.current = new fabric.Canvas(onionCanvasRef.current);
+    bgFabRef.current = new fabric.Canvas(bgCanvasRef.current);
+
+    return () => {
+      mainFabRef.current?.dispose();
+      onionFabRef.current?.dispose();
+      bgFabRef.current?.dispose();
+    };
+  }, []);
 
   return (
     <div>
-      <div className="text-blue-500">TIMELINE</div>
-      <div>CANVASes</div>
+      <div>TIMELINE</div>
+      <div className="grid grid-cols-1 grid-rows-1 w-[600px] h-[600px]">
+        <canvas ref={bgCanvasRef} />
+        <canvas ref={onionCanvasRef} />
+        <canvas ref={mainCanvasRef} />
+      </div>
       <div>PROPERTIES</div>
     </div>
   );
