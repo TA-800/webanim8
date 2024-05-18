b04effe9f1300b2c4c34565a1cf40ccf8f0fc18f
TA-800 <taherali0905@gmail.com>
2024-05-17 22:47:03 -0400

    set up canvas refs

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,16 @@
-import { useRef, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { fabric } from "fabric";
 
 function App() {
+  // Fabric refs
   const mainFabRef = useRef<fabric.Canvas | null>(null);
   const onionFabRef = useRef<fabric.Canvas | null>(null);
   const bgFabRef = useRef<fabric.Canvas | null>(null);
+
+  // Canvas references
+  const mainCanvasRef = useRef<HTMLCanvasElement | null>(null);
+  const onionCanvasRef = useRef<HTMLCanvasElement | null>(null);
+  const bgCanvasRef = useRef<HTMLCanvasElement | null>(null);
 
   return (
     <div>
